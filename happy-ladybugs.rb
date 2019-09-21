6571f37438e95294fa9769227c7a2b2bfcab1b04
BebopVinh <vinhdo07@gmail.com>
2019-09-20 21:25:00 -0700

    Initial commit

---
+++ happy-ladybugs.rb
@@ -0,0 +1,39 @@
+require 'pry'
+
+def happyLadybugs(b)
+    hash = {}
+    underscore_count = 0
+    hash.default = 0
+    #populate hash to store count for each character
+    b.each_char do |c|
+        unless c == "_"
+            hash[c] += 1
+        else   
+            underscore_count += 1
+        end
+    end
+
+    #special return cases
+    return "YES" if underscore_count > 0 && hash.empty?
+    return "NO" if hash.values.any?(1)
+
+    #check string to see if ladybugs are already happy without blank spaces
+    if underscore_count < 1
+      status = nil
+      i = 0
+      until i == b.size 
+         if i == 0 && b[i] != b[i + 1]
+            return "NO"
+         elsif b[i] != b[i + 1] && b[i] != b[i - 1]
+            return "NO"
+         end
+         i += 1
+      end
+    end
+
+    #check to see if ladybugs can be made happy
+    ans = (hash.values.any?(1))? "NO" : "YES"
+end
+
+
+happyLadybugs("RRRR")
